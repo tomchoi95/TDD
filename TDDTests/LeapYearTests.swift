0483b5c2deafadefc50654c82f3e9f363f0b01bd
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 17:33:33 +0900

    빨간색 단계: 실패하는 테스트 작성

--- TDDTests/LeapYearTests.swift
+++ TDDTests/LeapYearTests.swift
@@ -5,4 +5,10 @@
 //  Created by 최범수 on 2025-04-14.
 //
 
-import Foundation
+import XCTest
+
+class LeapYearTests: XCTestCase {
+    func testEvenlyDivisibleByFourIsLeap() {
+        XCTAssertTrue(isLeap(2020))
+    }
+}
