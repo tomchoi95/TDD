ea818142fe630fc06cf336a120a7eaa6c58d9175
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 16:22:01 +0900

    테스트 해볼 함수 작성.

---
+++ TDDTests/AssortedAssertionTest.swift
@@ -0,0 +1,42 @@
+//
+//  AssortedAssertionTest.swift
+//  TDDTests
+//
+//  Created by 최범수 on 2025-04-14.
+//
+
+import XCTest
+
+class AssortedAssertionTest: XCTestCase {
+    func testAssertNil() {
+        
+    }
+    
+    func testAssertNotNil() {
+        
+    }
+    
+    func testLessThan() {
+        
+    }
+    
+    func testGreaterThan() {
+        
+    }
+    
+    func testLessThanOrEqualTo() {
+        
+    }
+    
+    func testGreaterThanOrEqualTo() {
+        
+    }
+    
+    func testThrowsError() {
+        
+    }
+    
+    func testNoThorw() {
+        
+    }
+}
