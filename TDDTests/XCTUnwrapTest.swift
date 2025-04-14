287b27726294b38dc7030f5a18e075cfea5cc531
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 16:43:10 +0900

    XCTUnwrap 사용해 보기.

---
+++ TDDTests/XCTUnwrapTest.swift
@@ -0,0 +1,20 @@
+//
+//  XCTUnwrapTest.swift
+//  TDDTests
+//
+//  Created by 최범수 on 2025-04-14.
+//
+
+import XCTest
+
+struct User {
+    
+}
+
+class userRepository {
+    
+}
+
+class XCTUnwrapTest: XCTestCase {
+    
+}
