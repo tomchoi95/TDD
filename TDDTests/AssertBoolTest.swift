4e01eee38b27bf60e7be6e1d0058e9392a0cd6dc
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 16:06:29 +0900

    테스트 객제 준비.

--- TDDTests/AssertBoolTest.swift
+++ TDDTests/AssertBoolTest.swift
@@ -8,6 +8,21 @@
 import XCTest
 @testable import TDD
 
-class AssertBool: XCTestCase {
+final class AssertBool: XCTestCase {
+    
+    // 테스트 대상 클래스의 인스턴스 변수를 선언.
+    var assertBool: AssertBool!
+    
+    // 테스트 대상 객체 초기화.
+    override func setUp() {
+        super.setUp()
+        assertBool = AssertBool()
+    }
+    
+    // 테스트 실행 후 메모리 정리.
+    override func tearDown() {
+        assertBool = nil
+        super.tearDown()
+    }
     
 }
