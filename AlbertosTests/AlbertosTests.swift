dccec92f4909a209f186019946bce1f490afe104
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 19:16:45 +0900

    테스트 할 함수 먼저 정의.

--- AlbertosTests/AlbertosTests.swift
+++ AlbertosTests/AlbertosTests.swift
@@ -9,28 +9,36 @@ import XCTest
 @testable import Albertos
 
 final class AlbertosTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testEmptyMenuReturnsEmptySections() {
+        let menu = [MenuItem]()
+        let sections = groupMenuByCategory(menu)
+        XCTAssertTrue(sections.isEmpty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testMenuWithOneCategoryReturnsOneSection() {
+        let menu = [
+            MenuItem(category: "pastas", name: "name")
+            MenuItem(category: "pastas", name: "other name")
+        ]
+        let sections = groupMenuByCategory(menu)
+        XCTAssertEqual(sections.count, 1)
+        let section = try XCTUnwrap(sections.first)
+        XCTAssertEqual(section.items.count, 2)
+        XCTAssertEqual(section.items.first?.name, "name")
+        XCTAssertEqual(section.items.last?.name, "other name")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testMenuWithManyCategoriesReturnsManySectionsInReverseAlphabeticalOrder() {
+        let menu = [
+            MenuItem(category: "pastas",name: "a pasta"),
+            MenuItem(category: "drinks",name: "a drink"),
+            MenuItem(category: "pastas",name: "another pasta"),
+            MenuItem(category: "desserts",name: "a dessert")
+        ]
+        let sections = groupMenuByCategory(menu)
+        XCTAssertEqual(sections.count, 3)
+        XCTAssertEqual(sections[safe: 0]?.category, "pastas")
+        XCTAssertEqual(sections[safe: 1]?.category, "drinks")
+        XCTAssertEqual(sections[safe: 2]?.category, "desserts")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
