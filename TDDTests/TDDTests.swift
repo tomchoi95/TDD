96a7d943a335fcd950e6f79885f341351a1c4c35
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 15:13:59 +0900

    함수를 작성하였고, 실행해봄. 결과는 아래와 같음 Test Suite 'TDDTests' started at 2025-04-14 15:11:41.053. Test Case '-[TDDTests.TDDTests testFizzBuzzDivisibleBy15]' started. Test Case '-[TDDTests.TDDTests testFizzBuzzDivisibleBy15]' passed (0.001 seconds). Test Case '-[TDDTests.TDDTests testFizzBuzzDivisibleBy3]' started. Test Case '-[TDDTests.TDDTests testFizzBuzzDivisibleBy3]' passed (0.001 seconds). Test Case '-[TDDTests.TDDTests testFizzBuzzDivisibleBy5]' started. Test Case '-[TDDTests.TDDTests testFizzBuzzDivisibleBy5]' passed (0.001 seconds). Test Case '-[TDDTests.TDDTests testFizzBuzzNotDivisibleBy3Or5]' started. Test Case '-[TDDTests.TDDTests testFizzBuzzNotDivisibleBy3Or5]' passed (0.000 seconds). Test Suite 'TDDTests' passed at 2025-04-14 15:11:41.058.      Executed 4 tests, with 0 failures (0 unexpected) in 0.003 (0.005) seconds

--- TDDTests/TDDTests.swift
+++ TDDTests/TDDTests.swift
@@ -11,18 +11,26 @@ import XCTest
 final class TDDTests: XCTestCase {
     
     func testFizzBuzzDivisibleBy3() {
-        
+        let test = fizzBuzz(3)
+        let expected = "Fizz"
+        XCTAssertEqual(test, expected)
     }
     
     func testFizzBuzzDivisibleBy5() {
-        
+        let test = fizzBuzz(5)
+        let expected = "Buzz"
+        XCTAssertEqual(test, expected)
     }
     
     func testFizzBuzzDivisibleBy15() {
-        
+        let test = fizzBuzz(15)
+        let expected = "FizzBuzz"
+        XCTAssertEqual(test, expected)
     }
     
     func testFizzBuzzNotDivisibleBy3Or5() {
-        
+        let test = fizzBuzz(2)
+        let expected = "2"
+        XCTAssertEqual(test, expected)
     }
 }
