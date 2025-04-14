03de8a08fb279ba72cce3094a584ed8350edcbae
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 18:56:51 +0900

    AlbertosApp 추가.

---
+++ Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by 최범수 on 2025-04-14.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
