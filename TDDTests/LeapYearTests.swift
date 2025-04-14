fdbe20ca71c74e6e1af91375c73625f60048aa5c
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-14 17:47:52 +0900

    최종적으로 모든 조건을 만족하는지 알 수 있음.

--- TDDTests/LeapYearTests.swift
+++ TDDTests/LeapYearTests.swift
@@ -9,6 +9,7 @@ import XCTest
 
 class LeapYear {
     func isLeap(_ year: Int) -> Bool {
+        guard !(year % 400 == 0) else { return true }
         guard !(year % 100 == 0) else { return false }
         return year % 4 == 0
     }
