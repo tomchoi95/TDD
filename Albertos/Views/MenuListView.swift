26667c8422a00eb07060a69fe1a529be9f216f7b
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-15 11:02:39 +0900

    MenuList.ViewModel

--- Albertos/Views/MenuListView.swift
+++ Albertos/Views/MenuListView.swift
@@ -41,6 +41,18 @@ extension MenuRow {
     }
 }
 
+extension MenuList {
+    struct ViewModel {
+        let sections: [MenuSection]
+        
+        init(
+            menu: [MenuItem],
+            menuGrouping: @escaping ([MenuItem]) -> [MenuSection] = groupMenuByCategory) {
+            self.sections = menuGrouping(menu)
+        }
+    }
+}
+
 #Preview {
     MenuList(sections: groupMenuByCategory(.mockItems))
 }
