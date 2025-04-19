4bd7fdc057673208e6178806df61bd0ac4a4d974
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 13:51:11 +0900

    Initial Commit

---
+++ TDD/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  TDD
+//
+//  Created by 최범수 on 2025-04-19.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
