f8157da2ef2074369cb2c5089ef7121aae7e8309
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 15:00:29 +0900

    Mocking용 [MenuItem] 추가.

--- Albertos/AlbertosApp.swift
+++ Albertos/AlbertosApp.swift
@@ -11,7 +11,18 @@ import SwiftUI
 struct AlbertosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MenuList()
         }
     }
 }
+
+let menu = [
+    MenuItem(category: "starters", name: "Caprese Salad"),
+    MenuItem(category: "starters", name: "Arancini Balls"),
+    MenuItem(category: "pastas", name: "Penne all'Arrabbiata"),
+    MenuItem(category: "pastas", name: "Spaghetti Carbonara"),
+    MenuItem(category: "drinks", name: "Water"),
+    MenuItem(category: "drinks", name: "Red Wine"),
+    MenuItem(category: "desserts", name: "Tiramisù"),
+    MenuItem(category: "desserts", name: "Crema Catalana"),
+]
