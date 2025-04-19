99479455829fe5acd3172e6c143f00e6d78a9eb3
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 15:54:11 +0900

    MenuItem에 fixture라는 extension을 추가함. 더미데이터를 쉽게 추가할 수 있고, 더미데이터 정보를 중앙집중 관리할 수 있음.

--- Albertos/Models/MenuItem.swift
+++ Albertos/Models/MenuItem.swift
@@ -13,6 +13,15 @@ import Foundation
 struct MenuItem: Identifiable {
     let category: String
     let name: String
+    let spicy: Bool
+    let price: Double
     
     var id: String { name }
+}
+
+extension MenuItem {
+    static func fixture(category: String = "category", name: String = "name", spicy: Bool = false, price: Double = 0) -> MenuItem {
+        
+        return MenuItem(category: category, name: name, spicy: spicy, price: price)
+    }
 }
