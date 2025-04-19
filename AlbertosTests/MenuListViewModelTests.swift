804e2f9dc152a647fb9c4299beb08481a0bf91cb
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 19:15:20 +0900

    뷰모델 잘 작동하는지 테스트 코드로 테스트 해 보기.

--- AlbertosTests/MenuListViewModelTests.swift
+++ AlbertosTests/MenuListViewModelTests.swift
@@ -7,29 +7,22 @@
 
 import XCTest
 
+@testable import Albertos
+
 final class MenuListViewModelTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // grouping function이 잘 불려지는지 spyClosure를 대신 넣어보고 확인하기.
+    func testCallsGivenGroupingFunction() {
+        var isCalled = false
+        let inputSections = [MenuSection.fixture()]
+        let spyClosure: ([MenuItem]) -> [MenuSection] = { menuItems in
+            print("spy closure: \(menuItems)")
+            isCalled = true
+            return inputSections
         }
+        let viewModel = MenuList.ViewModel(menu: [.fixture()], menuGrouping: spyClosure)
+        let sections = viewModel.sections
+        
+        XCTAssertTrue(isCalled)
+        XCTAssertEqual(sections, inputSections)
     }
-
 }
