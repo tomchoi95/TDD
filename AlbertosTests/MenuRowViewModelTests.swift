c014d5c1217079e07ba560ccb46bdd01d21604f1
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 16:52:20 +0900

    테스트 통과.

--- AlbertosTests/MenuRowViewModelTests.swift
+++ AlbertosTests/MenuRowViewModelTests.swift
@@ -10,6 +10,23 @@ import XCTest
 @testable import Albertos
 
 final class MenuRowViewModelTests: XCTestCase {
-    func testWhenItemIsNotSpicyTextIsItemNameOnly() {}
-    func testWhenItemIsSpicyTextIsItemNameWithChiliEmoji() {}
+    // 뷰모델에서 안매운거 들어갔을 때 로우에서 이름만 출력되는지.
+    func testWhenItemIsNotSpicyTextIsItemNameOnly() {
+        // Arrange
+        let item = MenuItem.fixture(name: "name", spicy: false)
+        let viewModel = MenuRow.ViewModel(item: item)
+        
+        // Assert
+        XCTAssertEqual(viewModel.text, "name")
+    }
+    
+    // 뷰모델에서 매운거 들어갔을 때 로우에서 끝에 이모지가 붙어서 나오는지.
+    func testWhenItemIsSpicyTextIsItemNameWithChiliEmoji() {
+        // Arrange
+        let item = MenuItem.fixture(name: "spicy name", spicy: true)
+        let viewModel = MenuRow.ViewModel(item: item)
+        
+        // Assert
+        XCTAssertEqual(viewModel.text, "spicy name 🌶️")
+    }
 }
