fc3c662315e42eb3a79cfff616768c8cfdaf188f
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 19:14:49 +0900

    뷰모델 적용하기.

--- Albertos/Views/MenuList.swift
+++ Albertos/Views/MenuList.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
+extension MenuList {
+    class ViewModel: ObservableObject {
+        @Published var sections: [MenuSection]
+        
+        init(menu: [MenuItem], menuGrouping: @escaping ([MenuItem]) -> ([MenuSection]) = groupMenuByCategory ) {
+            self.sections = menuGrouping(menu)
+        }
+    }
+}
+
 struct MenuList: View {
-    let sections: [MenuSection]
+    @StateObject var viewModel: ViewModel
     
     var body: some View {
         //Initializer 'init(_:rowContent:)' requires that 'MenuSection' conform to 'Identifiable' -> 메뉴 섹션 Identifiable 만족하러 가자.
-        List(sections) { section in
+        List(viewModel.sections) { section in
             Section {
                 ForEach(section.items) { item in
                     Text(item.name)
@@ -25,5 +35,5 @@ struct MenuList: View {
 }
 
 #Preview {
-    MenuList(sections: groupMenuByCategory(menu))
+    MenuList(viewModel: .init(menu: menu))
 }
