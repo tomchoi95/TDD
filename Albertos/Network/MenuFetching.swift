85652847b361862a6711040e56cec0b50871f6e5
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 21:22:38 +0900

    MenuFetching 파일 정리.

--- Albertos/Network/MenuFetching.swift
+++ Albertos/Network/MenuFetching.swift
@@ -11,3 +11,38 @@ import Combine
 protocol MenuFetching {
     func fetchMenu() -> AnyPublisher<[MenuItem], Error>
 }
+
+// 네트워크 통신 실패를 내가 임의로 값을 보낼 수 있게 하는 Stub 객체.
+class MenuFetchingStub: MenuFetching {
+    private var result: Result<[MenuItem], Error>
+    
+    init(result: Result<[MenuItem], Error>) {
+        self.result = result
+    }
+    
+    func fetchMenu() -> AnyPublisher<[MenuItem], Error> {
+        result.publisher
+            .delay(for: 0.1, scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+class MenuFetchingPlaceholder: MenuFetching {
+    func fetchMenuAsync() async -> [MenuItem] {
+        try? await Task.sleep(nanoseconds: 500_000_000) // 0.5초 대기
+        
+        return menu
+    }
+    
+    func fetchMenu() -> AnyPublisher<[MenuItem], Error> {
+        
+        return Future<[MenuItem], Error> { [weak self] promise in
+            guard let self else { promise(.failure(NSError(domain: "", code: 0, userInfo: nil))); return }
+            Task {
+                let menu = await self.fetchMenuAsync()
+                promise(.success(menu))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
