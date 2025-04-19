334041f8b6ebcc43c7340a80405a7d58efcf4c7e
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-19 14:09:50 +0900

    delete all and initial commit

--- TDD/ContentView.swift
+++ TDD/ContentView.swift
@@ -22,28 +22,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-//
-//  ContentView.swift
-//  TDD
-//
-//  Created by 최범수 on 2025-04-14.
-//
-
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
-}
